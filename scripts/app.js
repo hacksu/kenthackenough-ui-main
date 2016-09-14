8240c0dfdba668f86cd85f880e05ed2b1964109e
Isaac Park <isaac.a.park@gmail.com>
2016-09-14 00:02:35 -0400

    Set to Html5Mode

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ angular
   .module('khe', ['ngCookies', 'btford.socket-io', 'ui.router', 'ngAnimate', 'angularMoment'])
   .constant('angularMomentConfig', {timezone: 'America/New_York'})
   .config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function ($locationProvider, $state, $route) {
-    $locationProvider.html5Mode(false);
+    $locationProvider.html5Mode(true);
 
     // Handle 404s
     $route.otherwise(function ($injector, $location) {
